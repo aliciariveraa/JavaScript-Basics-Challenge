4709743c84d841005d0ef719c4e820b50ef1eba9
aliciariveraa <aliciarivera@usf.edu>
2025-01-26 22:18:35 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,6 @@
+let employeeName = "John Mayor";
+const employeeID = 12575;
+var isActive = true;
+console.log(employeeName, typeof employeeName);
+console.log(employeeID, typeof employeeID);
+console.log(isActive, typeof isActive);
